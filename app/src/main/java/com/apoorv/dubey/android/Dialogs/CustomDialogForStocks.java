882e53eca8f582be1ed97ccf6d49ab803bc1acab
Abhishek Singh <abhishek111204cse@gmail.com>
2018-04-11 12:25:33 +0530

    Stocks Done

---
+++ app/src/main/java/com/apoorv/dubey/android/Dialogs/CustomDialogForStocks.java
@@ -0,0 +1,76 @@
+package com.apoorv.dubey.android.Dialogs;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.apoorv.dubey.android.instadia.R;
+import com.apoorv.dubey.android.model.Stock;
+
+
+public class CustomDialogForStocks extends Dialog implements
+        android.view.View.OnClickListener {
+
+    public Dialog dialog;
+    public Activity activity;
+    public DialogSaveClickedListener dialogSaveClicked;
+    public Button btnSave;
+    public Button btnCancel;
+    private EditText edtName;
+    private EditText edtItem;
+    private EditText edtInQuantity;
+    private EditText edtOutQuantity;
+
+    public CustomDialogForStocks(Activity activity,DialogSaveClickedListener fragment) {
+        super(activity);
+        this.activity = activity;
+        dialogSaveClicked = fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.custom_dialog_for_stocks);
+        btnSave = findViewById(R.id.btn_save);
+        btnCancel = findViewById(R.id.btn_cancel);
+        edtName = findViewById(R.id.edt_name);
+        edtItem = findViewById(R.id.edt_item);
+        edtInQuantity = findViewById(R.id.edt_in_quantity);
+        edtOutQuantity = findViewById(R.id.edt_out_quantity);
+        btnSave.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_save:
+                Stock stock = new Stock();
+                stock.setId(String.valueOf(System.currentTimeMillis()));
+                stock.setVendor(edtName.getText().toString());
+                stock.setItem(edtItem.getText().toString());
+                stock.setInQuantity(Integer.parseInt(edtInQuantity.getText().toString()));
+                stock.setOutQuantity(Integer.parseInt(edtOutQuantity.getText().toString()));
+                dialogSaveClicked.onDialogSaveButtonClicked(stock);
+                break;
+
+            case R.id.btn_cancel:
+                dismiss();
+            default:
+                break;
+        }
+        dismiss();
+    }
+
+    public interface DialogSaveClickedListener
+    {
+        void onDialogSaveButtonClicked(Stock stock);
+    }
+
+}
+
--- app/src/main/java/com/apoorv/dubey/android/instadia/StockActivity.java
+++ app/src/main/java/com/apoorv/dubey/android/instadia/StockActivity.java
@@ -1,17 +1,76 @@
 package com.apoorv.dubey.android.instadia;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class StockActivity extends AppCompatActivity {
+import com.apoorv.dubey.android.Adapter.StocksDataAdapter;
+import com.apoorv.dubey.android.Dialogs.CustomDialogForStocks;
+import com.apoorv.dubey.android.model.Stock;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
+
+public class StockActivity extends AppCompatActivity implements CustomDialogForStocks.DialogSaveClickedListener, View.OnClickListener {
+    private Button btnAddData;
+    private StocksDataAdapter stocksDataAdapter;
+    private RecyclerView recyclerView;
+    private List<Stock> stockDataList;
+    private DatabaseReference globaRef;
+    private ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock);
+        btnAddData =  findViewById(R.id.btn_add);
+        recyclerView = findViewById(R.id.recyclerView);
+        mProgressBar = findViewById(R.id.progress_bar);
+        mProgressBar.setVisibility(View.VISIBLE);
+        btnAddData.setOnClickListener(this);
+        stocksDataAdapter = new StocksDataAdapter(this,new ArrayList<Stock>());
+        stocksDataAdapter.setData(new ArrayList<Stock>());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, VERTICAL,false));
+        recyclerView.setAdapter(stocksDataAdapter);
+
+        // TODO: For reference as to how to store and show
+/*        globaRef = FirebaseDatabase.getInstance().getReferenceFromUrl(baseUrl + Constants.stocks);
+        Query myTopPostsQuery = globaRef;
+        myTopPostsQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                stockDataList = new ArrayList<>();
+                for (DataSnapshot stock: dataSnapshot.getChildren()) {
+                    stockDataList.add(stock.getValue(Stock.class));
+                }
+                stocksDataAdapter.setData(stockDataList);
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+            // ...
+        });*/
+
     }
 
     @Override
@@ -30,5 +89,23 @@ public class StockActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDialogSaveButtonClicked(Stock stock) {
+       //TODO Save data to firebase from here
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_add:
+                CustomDialogForStocks customDialog = new CustomDialogForStocks(getActivity(),this);
+                customDialog.setCancelable(false);
+                customDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                customDialog.show();
+                break;
+        }
     }
 }
