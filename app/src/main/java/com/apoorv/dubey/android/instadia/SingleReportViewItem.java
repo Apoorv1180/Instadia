1b028469df3ed37f24e549ee6d98c7f9c9db2814
arushi2503 <arushi2503@gmail.com>
2018-04-12 16:56:19 +0530

    stocks done part 2

---
+++ app/src/main/java/com/apoorv/dubey/android/instadia/SingleReportViewItem.java
@@ -0,0 +1,38 @@
+package com.apoorv.dubey.android.instadia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SingleReportViewItem extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView tvusername,tvdate,tvstand,tvproblemTYpe,tvproblemPlace,tvDescription;
+    //intent.putExtra("date",saveData.getDate());
+    //            intent.putExtra("userName",saveData.getUserName());
+    //            intent.putExtra("stand",saveData.getStand());
+    //            intent.putExtra("floor",saveData.getFloor());
+    //            intent.putExtra("work_category",saveData.getWork_category());
+    //            intent.putExtra("work_sub_category",saveData.getSub_workCategory());
+    //            intent.putExtra("pavilion",saveData.getPavallion());
+    //            intent.putExtra("houseKeepingPercentage",saveData.getHouseKeepingPercentage());
+    //            intent.putExtra("issuedescription",saveData.getIssueDescription());
+    //            intent.putExtra("completionstatus",saveData.getCompletionStatus());
+    //            intent.putExtra("photouri",saveData.getPhotoUri());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_report_view_item);
+        tvusername= findViewById(R.id.username);
+        tvdate=findViewById(R.id.date);
+        tvstand=findViewById(R.id.stand);
+        tvproblemPlace=findViewById(R.id.problem_place);
+        tvproblemTYpe=findViewById(R.id.problem_type);
+        tvDescription=findViewById(R.id.description);
+        imageView=findViewById(R.id.image_view);
+
+
+        
+    }
+}
